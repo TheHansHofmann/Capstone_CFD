39c77bdf6bc566c8250376b6ff66267141e35439
TheHansHofmann <hdhhofmann@gmail.com>
2022-11-17 17:51:22 -0700

    Updated Front End style and added some typescript vue coding examples

---
+++ server/routes/api/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+//this is just a placeholder file, will need to fill in later.
+
+//Get Post
+
+//Add Post
+
+//Delete Post
+
+module.exports = router;
+
